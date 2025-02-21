a7146ec2dae836fad1f0760b9f8617a2d5d77021
Jijith Jayakumar <jvjijith@live.com>
2025-02-21 21:02:50 +0530

    Nexa API CLient

---
+++ apps/web/src/nexa-api-client/apis/index.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppsApi';
+export * from './HealthApi';
---
+++ apps/web/src/nexa-api-client/models/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ColorModeResponse';
+export * from './ColorModeResponseTheme';
+export * from './ColorModeResponseThemePalette';
+export * from './ColorModeResponseThemePaletteAction';
+export * from './ColorModeResponseThemePaletteBackground';
+export * from './ColorModeResponseThemePalettePrimary';
+export * from './ColorModeResponseThemePaletteText';
+export * from './Get200Response';
+export * from './GetColorsByMode404Response';
+export * from './GetColorsByMode422Response';
+export * from './GetColorsByMode422ResponseError';
+export * from './GetColorsByMode422ResponseErrorIssuesInner';
+export * from './GetColorsByMode422ResponseErrorIssuesInnerPathInner';
+export * from './LayoutResponse';
+export * from './LayoutResponseFontInner';
+export * from './LayoutResponseFontSize';
+export * from './LayoutResponseLogosInner';
